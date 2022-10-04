35497321ada0ec7350c25a679acf38e945f55804
hugo fortunier <hfortunier@gmail.com>
2022-10-04 17:58:07 +0200

    game done

--- bowling.rb
+++ bowling.rb
@@ -1,7 +1,61 @@
 class Bowling
-    def roll(pins_down)
-    end
+  def initialize
+    @scores = []
+    @rolls = []
+    @bonus = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  end
 
-    def score()
+  def roll(pins_down)
+    if pins_down == 10
+      @rolls << pins_down
+      @rolls << 0
+    else
+      @rolls << pins_down
     end
+  end
+
+  def score()
+    sliced_array = @rolls.each_slice(2).to_a
+
+    sliced_array.each_with_index do |element, index|
+      if index != 9
+        standard_case(element, index)
+      else
+        last_roll_case(element, index)
+      end
+    end
+    final_score = 0
+    @scores.each_with_index do |value, index|
+      final_score += value*@bonus[index]
+    end
+  final_score
+  end
+
+  private
+
+  def standard_case(element, index)
+    if element.include? 0
+      @scores << element.sum
+      @bonus[index + 1] += 1
+      @bonus[index + 2] += 1
+    elsif element.sum == 10
+      @scores << element.sum
+      @bonus[index + 1] += 1
+    else
+      @scores << element.sum
+    end
+  end
+
+  def last_roll_case(element, index)
+    if element.include? 0
+      @scores << element.sum
+      @scores << 10
+      @scores << 10
+    elsif element.sum == 10
+      @scores << element.sum
+      @scores << 5
+    else
+      @scores << element.sum
+    end
+  end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Bowling, "@score" do
         expect(bowling.score()).to eq 80
       end
     end
+
+    context "with spares" do
+      it "sum the pin count for each roll" do
+        bowling = Bowling.new
+        10.times do
+          bowling.roll(4)
+          bowling.roll(6)
+        end
+
+        expect(bowling.score()).to eq 195
+      end
+    end
+
+    context "with strikes" do
+      it "sum the pin count for each roll" do
+        bowling = Bowling.new
+        10.times do
+          bowling.roll(10)
+        end
+
+        expect(bowling.score()).to eq 300
+      end
+    end
 end
